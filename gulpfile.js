157173581d4f17a30229adada2c4a849a151a6d7
Julian <julfleischer@paypal.com>
2015-04-02 00:33:53 +0200

    coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-// vim: set et sw=2 ts=2
-(function () { "use strict";
+/* vim: set et sw=2 ts=2: */
+/* jshint node: true */
+"use strict";
 
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var enforcer = require('gulp-istanbul-enforcer');
 
 var sourceFiles = [ '*.js', 'lib/*.js', 'test/*.js' ];
 
@@ -14,6 +18,26 @@ gulp.task('lint', function (done) {
       .on('end', done);
 });
 
-gulp.task('default', [ 'lint' ]);
+gulp.task('test', function (done) {
+  gulp.src(sourceFiles)
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire()) 
+      .on('finish', function () {
+        gulp.src([ 'test/*.js' ])
+            .pipe(mocha())
+            .pipe(istanbul.writeReports())
+            .pipe(enforcer({
+              thresholds: {
+                statements: 60,
+                branches: 60,
+                lines: 60,
+                functions: 60
+              },
+              coverageDirectory: 'coverage',
+              rootDirectory: ''
+            }))
+            .on('end', done);
+      });
+});
 
-}());
+gulp.task('default', [ 'lint', 'test' ]);
