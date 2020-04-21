324139997a319217fa221a5c80838b0b9718cbde
Julian Fleischer <tirednesscankill@warhog.net>
2020-04-21 20:38:58 +0200

    More recent node versions

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,6 @@
 /* jshint node: true */
 "use strict";
 
-/*
-var thresholds = {
-  statements: 90,
-    branches: 80,
-       lines: 90,
-   functions: 90
-};
-
-var jshint = require('gulp-jshint'),
-     mocha = require('gulp-mocha'),
-  istanbul = require('gulp-istanbul'),
-  enforcer = require('gulp-istanbul-enforcer'),
-      gulp = require('gulp');
-
-var sourceFiles = [ '*.js', 'lib/*.js', 'test/*.js' ];
-
-gulp.task('lint', function () {
-  return gulp.src([ 'index.js', 'test/*.js', 'benchmark/*.js' ])
-             .pipe(jshint())
-             .pipe(jshint.reporter('default'))
-});
-
-gulp.task('test', [ 'lint' ], function (done) {
-  gulp.src('index.js')
-      .pipe(istanbul())
-      .pipe(istanbul.hookRequire()) 
-      .on('finish', function () {
-        gulp.src('test/*.js')
-            .pipe(mocha())
-            .pipe(istanbul.writeReports())
-            .pipe(enforcer({
-              thresholds: thresholds,
-              coverageDirectory: 'coverage',
-              rootDirectory: ''
-            }))
-            .on('end', done);
-    });
-});
-
-gulp.task('default', [ 'test' ]);
-*/
-
 const { series, src, dest } = require('gulp');
 const jshint = require('gulp-jshint');
 const mocha = require('gulp-mocha');
